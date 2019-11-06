67302df83c53db8cc9690efc1a7050386117f904
Khris Punzalan <khrispunzalan@gmail.com>
2019-11-06 05:32:42 -0800

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: :all_blank
 
 end
+
+# reject_if:
+# silently ignore any new record hashes if they fail to pass your criteria
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,20 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  # accepts_nested_attributes_for :categories
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attr|
+    # binding.pry
+      if !category_attr[:name].empty? 
+        # category_attr[:name] != "" 
+        category = Category.find_or_create_by(category_attr)
+        self.categories << category     
+        # self.save
+      end
+    end
+  end
 end
+
+# self.categories << category  
+# establishes a new relation between a post and a category
