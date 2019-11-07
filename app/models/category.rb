c447654b238dfbf3f85eda69fb374a7f4f705bf0
Khris Punzalan <khrispunzalan@gmail.com>
2019-11-06 20:45:26 -0800

    Done.

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ActiveRecord::Base
   has_many :post_categories
   has_many :posts, through: :post_categories
 
-  # validates :name, uniqueness: true
+  validates :name, uniqueness: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -12,7 +12,8 @@ class Post < ActiveRecord::Base
       if !category_attr[:name].empty? 
         # category_attr[:name] != "" 
         category = Category.find_or_create_by(category_attr)
-        self.categories << category     
+        # self.categories << category
+        self.post_categories.build(:category => category)     
         # self.save
       end
     end
@@ -20,4 +21,18 @@ class Post < ActiveRecord::Base
 end
 
 # self.categories << category  
-# establishes a new relation between a post and a category
+  # establishes a new relation between a post and a category
+  # ineffiecient
+
+
+# from has has_many video:
+# def categories_attributes=(categories_hashes)
+#     categories_hashes.each do |i, category_attributes|
+#       if categories_attributes[:name].present?
+#         category = Category.find_or_create_by(name: categories_attributes[:name])
+#         if !self.categories.include?(category)
+#         self.post_categories.build(:category => category)
+#       end
+#     end
+#   end
+# end
